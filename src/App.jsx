d574b5a9a0d94413c4bef39899614992e99a4fb5
JP <jp@JPPC.localdomain>
2023-08-10 21:04:55 -0700

    added ContactList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import  ContactList  from './components/ContactList'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-        
+        <ContactList />
       </div>
     </>
   )
