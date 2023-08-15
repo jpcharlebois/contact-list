a28dd12f7239fa029ab9a830285634abaa8e6b4f
JP <jp@JPPC.localdomain>
2023-08-15 15:38:46 -0700

    added setSelectedContactId to props in ContactRow from parent App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import  ContactList  from './components/ContactList'
+//import SelectedContact from "./components/SelectedContact"
 import './App.css'
 
 function App() {
-
+  let [selectedContactId, setSelectedContactId] = useState(null);
   return (
     <>
-      <div>
-        <ContactList />
-      </div>
+      {selectedContactId ? (
+        <div>
+          Selected Contact View
+        </div>
+      ) : (
+      <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   )
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -7,7 +7,7 @@ const dummyContacts = [
     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
 
-export default function ContactList() {
+export default function ContactList({ setSelectedContactId}) {
     const [contacts, setContacts] = useState(dummyContacts);
     console.log("Contacts: ", contacts);
     useEffect(()=>{
@@ -38,7 +38,7 @@ export default function ContactList() {
                 </tr>
                 {
                     contacts.map((contact) => {
-                        return <ContactRow key={contact.id} contact={contact} />;
+                        return <ContactRow key={contact.id} setSelectedContactId={setSelectedContactId} contact={contact} />;
                     })
                 }
             </tbody>
