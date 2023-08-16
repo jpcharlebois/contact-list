26b8d75ee8bf3429e24486447613942d204f3635
JP <jp@JPPC.localdomain>
2023-08-15 17:39:44 -0700

    fixed SelectedContact, passing in selectedContactId

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 
+const contact1 = { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" };
 export default function SelectedContact({ selectedContactId, setSelectedContactId }) {
-    const [contact, setContact] = useState(null);
+    const [contact, setContact] = useState(contact1);
     console.log("Contact: ", contact);
     useEffect(()=>{
         async function fetchContact(selectedContactId) {
@@ -14,7 +15,7 @@ export default function SelectedContact({ selectedContactId, setSelectedContactI
                 console.error(error);
             }
         }
-        fetchContact()
+        fetchContact(selectedContactId)
     },[]);
     return (
         <div>
